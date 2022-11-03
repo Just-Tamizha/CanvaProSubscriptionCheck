29c9a6fe90d55d050b8b9cbff13930a150a2a57a
Just-Tamizha <protamizhahelp@gmail.com>
2022-11-03 20:07:28 +0530

    Change Code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-// document. addEventListener( "DOMContentLoaded",init());
 function init(getMail){
     const sheetID ='14y4OxGf5wXix4E_qCOGrwBMpEluT35FbmITZtTaFDx0';
     const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
@@ -26,19 +24,16 @@ function init(getMail){
             }else{
                 document.getElementById("status").innerHTML=`<button type="button" class="btn btn-danger btn-sm">${rows[0].c[3].v}</button>`
             }
-            // document.getElementById("goto").innerHTML=""
             if(rows[0].c[3].v =='Active'){
                 document.getElementById("goto").innerHTML=`<a class="btn btn-success btn-sm" href="${rows[0].c[4].v}" role="button">Go to Canva</a>`
             }else{
-                alert("Your Canva Pro Subscription is Expired! Go to Purchase on Etsy")
-                document.getElementById("goto").innerHTML=`<a class="btn btn-success btn-sm" href="https://www.etsy.com/in-en/listing/1311639192/canva-pro-canva-pro-for-low-price?ref=listings_manager_grid" role="button">Go to Etsy</a>`
+                document.getElementById("goto").innerHTML=`<a class="btn btn-success btn-sm" href="${rows[0].c[4].v}" role="button">Go to Etsy</a>`
             }
             document.getElementById("userDetails").className="d-flex justify-content-center d-block"
             
         }else{
             alert("User Not Found! Contact Admin")
         }
-        // return js0.table.rows[0].c;
     })
 }
 function ValidateEmail() {
